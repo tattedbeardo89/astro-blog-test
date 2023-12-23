a10f508a0d9bdada57d3704fa0debcac8ae45473
tattedbeardo89 <rtuc124@wgu.edu>
2023-12-22 20:32:53 -0600

    final commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://tattedbeardo89-astro-blog-test.netlify.app"
+  site: "https://tattedbeardo89-astro-blog-test.netlify.app",
+  integrations: [preact()]
 });
