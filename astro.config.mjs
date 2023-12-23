9a41cf228ed3180496ef1cc17d92692c4fb5ff7b
tattedbeardo89 <rtuc124@wgu.edu>
2023-12-22 20:24:26 -0600

    added xml 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
-site: "https://tattedbeardo89-astro-blog-test.netlify.app/"
+export default defineConfig({
+  site: "https://tattedbeardo89-astro-blog-test.netlify.app"
+});
